3979af5515d45a57594834ec11bd2adc88541de0
techhtml <apes0123@gmail.com>
2022-07-14 21:43:33 +0900

    CORS 추가

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express, {Request, Response} from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import morgan from 'morgan';
 import {initializeApp} from 'firebase-admin/app';
 import {MessageRouter} from './router/MessageRouter';
 
 const app = express();
+app.use(cors({origin: 'http://localhost:3000'}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('combined'));
