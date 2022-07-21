fd4f3b7c8525707b237799201dba7abdfde4bf2e
techhtml <apes0123@gmail.com>
2022-07-21 21:36:30 +0900

    ✨ 예외 상황 처리

--- src/router/PostRouter/post.ts
+++ src/router/PostRouter/post.ts
@@ -3,8 +3,21 @@ import {Request, Response} from 'express';
 
 export const writePost = async (req: Request, res: Response) => {
   const {title, content} = req.body;
+  try {
+    if (!title) {
+      res.status(400).send('Title은 필수값입니다.');
+      return;
+    }
 
-  const ref = firestore().collection('posts');
-  await ref.add({title, content});
-  res.status(200).send('게시글 작성 완료');
+    if (!content) {
+      res.status(400).send('Description은 필수값입니다.');
+      return;
+    }
+
+    const ref = firestore().collection('posts');
+    await ref.add({title, content});
+    res.status(200).send('게시글 작성 완료');
+  } catch (error) {
+    res.status(400).send(error);
+  }
 };
